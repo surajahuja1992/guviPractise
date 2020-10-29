18154371ae26f57e8ec6759462d9e6018a336fc6
surajahuja1992 <31279046+surajahuja1992@users.noreply.github.com>
2020-10-29 09:06:23 +0530

    Add files via upload

---
+++ Day14_October28/script.js
@@ -0,0 +1,42 @@
+let master = document.getElementById('master');
+
+function createTimer() {
+    setTimeout(function () {
+        setTimeout(function () {
+            setTimeout(function () {
+                setTimeout(function () {
+                    setTimeout(function () {
+                        setTimeout(function () {
+                            setTimeout(function () {
+                                setTimeout(function () {
+                                    setTimeout(function () {
+                                        setTimeout(function () {
+
+                                            let divChild = document.createElement('div');
+                                            divChild.innerHTML = 'Happy Independence Day';
+                                            document.body.innerHTML = '';
+                                            document.body.appendChild(divChild);
+                                        }, 1000);
+                                        master.innerHTML = 1;
+                                    }, 1000);
+                                    master.innerHTML = 2;
+                                }, 1000);
+                                master.innerHTML = 3;
+                            }, 1000);
+                            master.innerHTML = 4;
+                        }, 1000);
+                        master.innerHTML = 5;
+                    }, 1000);
+                    master.innerHTML = 6;
+                }, 1000);
+                master.innerHTML = 7;
+            }, 1000);
+            master.innerHTML = 8;
+        }, 1000);
+        master.innerHTML = 9;
+    }, 1500);
+    master.innerHTML = 10;
+
+}
+
+createTimer();
